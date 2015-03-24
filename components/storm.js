6e6fd2d2ab2060e0f6173e9071dde317913c1c82
Luca Capra <luca.capra@gmail.com>
2015-03-24 11:32:59 +0100

    use symlink for dispatcher jar

--- components/storm.js
+++ components/storm.js
@@ -52,13 +52,14 @@ lib.start = function() {
     var out = require('fs').openSync(lib.info.logFile, 'w');
 
     console.log("cwd is ", lib.info.stormHome);
-    console.log("Launch ", [lib.info.command, "jar", lib.info.dispatcherHome + "/dispatcher-0.4.1-jar-with-dependencies.jar",
+    console.log("Launch ", [lib.info.command, "jar", lib.info.dispatcherHome + "/dispatcher.jar",
         "com.servioticy.dispatcher.DispatcherTopology",
         "-f", lib.info.dispatcherHome + "/dispatcher.xml",
-        "-d"].join(' '));
+        "-d"
+    ].join(' '));
 
     return util.launch(lib.info.command, [
-        "jar", lib.info.dispatcherHome + "/dispatcher-0.4.1-jar-with-dependencies.jar",
+        "jar", lib.info.dispatcherHome + "/dispatcher.jar",
         "com.servioticy.dispatcher.DispatcherTopology",
         "-f", lib.info.dispatcherHome + "/dispatcher.xml",
         "-d"
